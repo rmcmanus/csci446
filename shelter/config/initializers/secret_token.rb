4ef703b701c2154413541f5c2d4e8609f4491f70
Ryan McManus <rmcmanus@mines.edu>
2014-02-18 16:09:43 -0700

    Created shelter for Unit 5

---
+++ shelter/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shelter::Application.config.secret_key_base = '211d0072a8a75549ba02a9f2609d4a92ca58125527eecc416e1f4cdc31c04db572148e5a48daba60e0478547c33263052271f035f934b0ba11c98092e2cb911c'
